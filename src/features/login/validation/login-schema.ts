85bf2cd3f0ea2ce7d1ae8004626e44bc81d5684a
Odelola <integralandintegrations@gmail.com>
2026-06-17 18:50:24 +0100

    setup: production hotfix

--- src/features/login/validation/login-schema.ts
+++ src/features/login/validation/login-schema.ts
@@ -10,16 +10,16 @@ export const loginSchema = yup.object().shape({
     .required('Password is required.')
     .min(8, 'Password must be at least 8 characters long.')
     .max(64, 'Password must not exceed 64 characters.')
-    .matches(/[a-z]/, {
-      message: 'Password must contain at least one lowercase letter.',
-    })
-    .matches(/[A-Z]/, {
-      message: 'Password must contain at least one uppercase letter.',
-    })
-    .matches(/\d/, {
-      message: 'Password must contain at least one number.',
-    })
-    .matches(/[^A-Za-z0-9]/, {
-      message: 'Password must contain at least one special character.',
-    }),
+    // .matches(/[a-z]/, {
+    //   message: 'Password must contain at least one lowercase letter.',
+    // })
+    // .matches(/[A-Z]/, {
+    //   message: 'Password must contain at least one uppercase letter.',
+    // })
+    // .matches(/\d/, {
+    //   message: 'Password must contain at least one number.',
+    // })
+    // .matches(/[^A-Za-z0-9]/, {
+    //   message: 'Password must contain at least one special character.',
+    // }),
 });
